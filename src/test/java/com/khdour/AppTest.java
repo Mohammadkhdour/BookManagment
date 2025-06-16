61e6bba789719fb0e1d5920549013713bda79668
Mohammadkhdour <mohammadkhdour18@gmail.com>
2025-06-17 02:49:42 +0300

    add simple unit test

--- src/test/java/com/khdour/AppTest.java
+++ src/test/java/com/khdour/AppTest.java
@@ -1,6 +1,6 @@
 package com.khdour;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -16,5 +16,20 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void searchBookwithIBN1211()
+    {  
+        // setup
+        BookStore store = new BookStore();
+        Book book1211 = new Book("1211", "hello", "khdour", 20);
+        store.addBook(book1211);
+        //exercise
+        Book searched = store.searchByISBN("1111");
+        //verify
+        assertEquals( book1211,searched );
+        //tear down
+        store.deleteByISBN("1211");
     }
 }
